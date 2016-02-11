d985172a82126bfe3f2cebb7e22fbf62d73f0807
Hack Reactor Students <accounts@hackreactor.com>
2016-02-11 15:19:31 -0800

    Display all links and clean up ghost code

--- app/config.js
+++ app/config.js
@@ -1,14 +1,9 @@
 var path = require('path');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/shortly');
-  // client: 'sqlite3',
-  // connection: {
-  //   filename: path.join(__dirname, '../db/shortly.sqlite')
-  // }
-// });
-// var db = require('bookshelf')(knex);
 var db = mongoose.connection;
 var Schema = mongoose.Schema;
+
+mongoose.connect('mongodb://127.0.0.1:27017/shortly');
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -23,48 +18,15 @@ var urlsSchema = new Schema({
   visits: Number
 }, {collection: 'urls'});
 
-var Url = mongoose.model('Url', urlsSchema);
-
 var usersSchema = new Schema({
   username: {type: String, required: true, unique: true},
   password: String,
 }, {collection: 'users'});
 
+var Url = mongoose.model('Url', urlsSchema);
 var User = mongoose.model('User', usersSchema);
 
-// var user1 = new User({username:'fred2', password:'pass'});
-
-// user1.save(function (err, user1) {
-//   if (err) return console.error(err);
-// });
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('baseUrl', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
 module.exports = db;
 module.exports.User = User;
 module.exports.Url = Url;
+module.exports.urls = urlsSchema;
